f1e4ee7d3001da6b0616a2fb92c397511f5e18cb
david anderson <d.a.m.anderson@gmail.com>
2016-10-24 14:53:23 +0100

    fizzbuzz working model and passing tests

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,3 +4,18 @@ var FizzBuzz = function(){
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
 };
+
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
+
+FizzBuzz.prototype.play = function(number) {
+  if (this.isDivisibleByThree(number) && this.isDivisibleByFive(number)) {
+    return 'fizzbuzz';}
+  else if (this.isDivisibleByThree(number)) {
+    return 'fizz';}
+  else if (this.isDivisibleByFive(number)) {
+    return 'buzz';}
+  else {
+    return number;}
+};
